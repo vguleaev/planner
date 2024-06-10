e086fb75b387f8c171bc377bcd4f675e3822988c
Vladislav Guleaev <Vladislav.Guleaev@check24.de>
2024-06-10 23:03:11 +0200

    Add tailwind and shadcn ui

--- README.md
+++ README.md
@@ -1,27 +1,43 @@
 # Planner
 
-To install dependencies:
+To install dependencies server:
 
 ```bash
 bun install
 ```
 
-To run:
+To install dependencies frontend:
 
 ```bash
-bun run index.ts
+cd frontend && bun install
 ```
 
 This project was created using `bun init` in bun v1.1.7. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+
+## Run locally
+
+You need to run both frontend and backend separately.
+
+Run backend:
+`bun run dev`
+
+Run frontend:
+`cd frontend && bun run dev`
+
+Open http://localhost:5173/
 
 ## Build in Docker
 
 Docker build:
 
-`docker build . -t planner-app`
+```bash
+docker build . -t planner-app
+```
 
 Docker run:
 
-`docker run --init -p 3000:3000 planner-app`
+```bash
+docker run --init -p 3000:3000 planner-app
+```
 
 _(--init is needed to handle Ctrl +C SIGINT signals properly)_
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,22 +18,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-col">
+        <button className="bg-green-100 mr-5" onClick={() => setCount((count) => count + 1)}>
+          count is {count} UP
+        </button>
+        <button className="bg-red-100" onClick={() => setCount((count) => count - 1)}>
+          count is {count} DOWN
+        </button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
