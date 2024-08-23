6fa1c095b4d7346ffce3e54dd445da586393f3c5
Vladislav Guleaev <Vladislav.Guleaev@check24.de>
2024-08-23 23:42:58 +0200

    Add console log to test db connection

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,5 +1,6 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
+console.log('Connecting to database at:', process.env.DATABASE_URL);
 const client = postgres(process.env.DATABASE_URL!);
 export const db = drizzle(client);
