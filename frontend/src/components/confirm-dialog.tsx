d6afec52fac11b5b50598e4e2ab5406171359015
Vladislav Guleaev <Vladislav.Guleaev@check24.de>
2024-09-08 11:52:07 +0200

    Refactor confirm dialog, make it controlled dialog, async and show spinner on button

--- frontend/src/components/confirm-dialog.tsx
+++ frontend/src/components/confirm-dialog.tsx
@@ -6,20 +6,41 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
+import { useState } from 'react';
+import { Button } from './ui/button';
+import { LoaderCircle } from 'lucide-react';
 
-export const ConfirmDialog = ({ onConfirm, children }: { onConfirm: () => void; children: React.ReactNode }) => {
+interface Props {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  onConfirm: () => void;
+}
+
+export const ConfirmDialog = ({ open, onConfirm, onOpenChange }: Props) => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onConfirmClick = async () => {
+    setIsSubmitting(true);
+    await onConfirm();
+    onOpenChange(false);
+    setIsSubmitting(false);
+  };
+
   return (
-    <AlertDialog>
-      <AlertDialogTrigger>{children}</AlertDialogTrigger>
+    <AlertDialog open={open} onOpenChange={onOpenChange}>
       <AlertDialogContent>
         <AlertDialogHeader>
           <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
         </AlertDialogHeader>
         <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction onClick={onConfirm}>Confirm</AlertDialogAction>
+          <Button variant="outline" onClick={() => onOpenChange(false)} disabled={isSubmitting}>
+            Cancel
+          </Button>
+          <Button variant="default" onClick={onConfirmClick} disabled={isSubmitting}>
+            {isSubmitting && <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />}
+            Confirm
+          </Button>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
