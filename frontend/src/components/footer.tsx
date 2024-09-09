6e2da55b5abc603f78a23f8d96cf4c3381e9b3e4
Vladislav Guleaev <Vladislav.Guleaev@check24.de>
2024-09-09 20:59:39 +0200

    Add license

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <footer className="w-full bg-background py-6 px-4">
       <div className="container mx-auto px-4">
         <div className="text-center text-sm">
-          <p>&copy; {new Date().getFullYear()} Planner. All rights reserved.</p>
+          <p className="text-foreground/40">&copy; {new Date().getFullYear()} Planner. All rights reserved.</p>
         </div>
       </div>
     </footer>
