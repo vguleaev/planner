0b6bd1bfb55e51cc15cb93c8169e8256dfca4563
Vladislav Guleaev <Vladislav.Guleaev@check24.de>
2024-10-13 15:05:13 +0200

    Fix navbar z-index

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -19,7 +19,7 @@ export function Navbar() {
   const { data } = useCurrentUser();
 
   return (
-    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+    <header className="sticky z-[1] top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link to="/backlog" className="flex items-center gap-2 text-lg font-semibold md:text-base">
           <Logo className="h-7 w-7 mr-2"></Logo>
